8e1b2b1155b4f22464297a3017e41bdc6172db67
harshmangalam <harshdev8218@gmail.com>
2023-12-16 17:17:05 +0530

    Added prisma client

---
+++ src/lib/prisma.ts
@@ -0,0 +1,16 @@
+import {PrismaClient} from "@prisma/client"
+import { PrismaLibSQL } from '@prisma/adapter-libsql'
+import { createClient } from '@libsql/client'
+
+// Create a new instance of the libSQL database client
+const libsql = createClient({
+  // @ts-expect-error
+  url: process.env.TURSO_DATABASE_URL,
+  authToken: process.env.TURSO_AUTH_TOKEN
+})
+
+// Create a Prisma "adapter" for libSQL
+const adapter = new PrismaLibSQL(libsql)
+// Pass the adapter option to the Prisma Client instance
+export const prisma = new PrismaClient({ adapter })
+
