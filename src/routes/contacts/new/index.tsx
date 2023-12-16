7b82eff1cd59ebc59933b74e8116e24a47f10583
harshmangalam <harshdev8218@gmail.com>
2023-12-16 18:04:56 +0530

    added create contact form

--- src/routes/contacts/new/index.tsx
+++ src/routes/contacts/new/index.tsx
@@ -1,5 +1,8 @@
 import { component$ } from "@builder.io/qwik";
 import { routeAction$, zod$, z, Form, Link } from "@builder.io/qwik-city";
+import { Button } from "~/components/ui/button";
+import { TextInput } from "~/components/ui/text-input";
+import { Textarea } from "~/components/ui/textarea";
 import { prisma } from "~/lib/prisma";
 
 export const useAddContact = routeAction$(
@@ -16,78 +19,40 @@ export const useAddContact = routeAction$(
     twitter: z.string(),
     avatarUrl: z.string(),
     notes: z.string(),
-  })
+  }),
 );
 export default component$(() => {
-  const addContactAction = useAddContact();
-  const firstNameError = addContactAction.value?.fieldErrors?.firstName;
-
   return (
-    <section class="min-h-screen grid place-items-center">
-      <Form
-        action={addContactAction}
-        class="grid grid-cols-2 gap-4 max-w-md w-full mx-auto px-4 md:px-0"
-      >
-        <section class="col-span-1 flex flex-col space-y-2">
-          <label for="firstName">First name</label>
-          <input type="text" name="firstName" id="firstName" />
-          {firstNameError?.length ? (
-            <span class="text-red-500 text-sm">{firstNameError[0]}</span>
-          ) : null}
-        </section>
-        <section class="col-span-1 flex flex-col space-y-2">
-          <label for="lastName">Last name</label>
-          <input
-            value={addContactAction.formData?.get("lastName") ?? ""}
-            type="text"
-            name="lastName"
-            id="lastName"
-          />
-        </section>
-        <section class="col-span-2 flex flex-col space-y-2">
-          <label for="twitter">Twitter</label>
-          <input
-            value={addContactAction.formData?.get("twitter") ?? ""}
-            type="text"
+    <article class="card card-bordered mx-auto max-w-xl">
+      <Form class="card-body">
+        <header class="card-title">New Contact</header>
+        <div>
+          <TextInput name="name" id="name" label="Full Name" error={""} />
+          <TextInput
             name="twitter"
             id="twitter"
+            label="Twitter handler"
+            error={""}
           />
-        </section>
-        <section class="col-span-2 flex flex-col space-y-2">
-          <label for="avatarUrl">Avatar URL</label>
-          <input
-            value={addContactAction.formData?.get("avatarUrl") ?? ""}
-            type="text"
-            name="avatarUrl"
-            id="avatarUrl"
+          <TextInput
+            name="twitter"
+            id="twitter"
+            label="Twitter handler"
+            error={""}
           />
-        </section>
-        <section class="col-span-2 flex flex-col space-y-2">
-          <label for="notes">Notes</label>
-          <textarea
-            value={(addContactAction.formData?.get("notes") as string) ?? ""}
-            name="notes"
-            id="notes"
-            rows={5}
-          />
-        </section>
+          <TextInput name="avatar" id="avatar" label="Avatar URL" error={""} />
+          <Textarea name="notes" id="notes" label="Notes" error={""} />
+        </div>
 
-        <section class="flex space-x-2 items-center">
-          <button
-            type="submit"
-            disabled={addContactAction.isRunning}
-            class="bg-white border text-blue-500 shadow px-2 py-2 rounded-md disabled:bg-gray-100 disabled:text-gray-500"
-          >
-            Save
-          </button>
-          <Link
-            href="/"
-            class="bg-white border text-red-500 shadow px-2 py-2 rounded-md"
-          >
+        <footer class="card-actions">
+          <Link class="btn btn-error" href={"/"}>
             Cancel
           </Link>
-        </section>
+          <Button type={"submit"} colorScheme={"btn-primary"}>
+            Create
+          </Button>
+        </footer>
       </Form>
-    </section>
+    </article>
   );
 });
