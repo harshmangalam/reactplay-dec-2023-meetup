99f282c5b7e179be45eaef71bf361914acaef49c
harshmangalam <harshdev8218@gmail.com>
2023-12-16 18:40:54 +0530

    added contacts loader

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -2,74 +2,65 @@ import { type Signal, component$ } from "@builder.io/qwik";
 import { Link, useLocation } from "@builder.io/qwik-city";
 import { ChevronLeftIcon } from "~/icons/chevron-left";
 import { Search } from "./search";
+import { useContacts } from "~/routes/layout";
 
-export const Sidebar = component$(
-  ({
-    drawer,
-    contacts,
-  }: {
-    drawer: Signal<boolean>;
-    contacts: { id: string; firstName: string; lastName: string | null }[];
-  }) => {
-    const loc = useLocation();
+export const Sidebar = component$(({ drawer }: { drawer: Signal<boolean> }) => {
+  const contacts = useContacts();
+  const loc = useLocation();
 
-    return (
-      <aside
-        class={`${
-          !drawer.value ? "hidden md:flex" : ""
-        } absolute md:static md:max-w-xs w-full flex flex-col divide-y bg-gray-100 border-r h-screen`}
-      >
-        <header class="z-10 py-2 px-2 md:px-6 flex items-center gap-2">
-          <Search />
-          <a
-            href="/contacts/new"
-            class="bg-white border text-blue-500 shadow px-2 py-2 rounded-md"
-          >
-            New
-          </a>
-        </header>
+  return (
+    <aside
+      class={`${
+        !drawer.value ? "hidden md:flex" : ""
+      } absolute flex h-screen w-full flex-col divide-y border-r bg-gray-100 md:static md:max-w-xs`}
+    >
+      <header class="z-10 flex items-center gap-2 px-2 py-2 md:px-6">
+        <Search />
+        <a
+          href="/contacts/new"
+          class="rounded-md border bg-white px-2 py-2 text-blue-500 shadow"
+        >
+          New
+        </a>
+      </header>
 
-        <section class="flex-1 overflow-y-auto px-2 md:px-6 py-4">
-          {contacts.length ? (
-            <ul class="h-full flex flex-col space-y-2">
-              {contacts.map((contact) => (
-                <li key={contact.id}>
-                  <Link
-                    href={`/contacts/${contact.id}`}
-                    class={`py-2 block rounded-md px-2  ${
-                      loc.url.pathname === `/contacts/${contact.id}/`
-                        ? "bg-blue-500 text-white "
-                        : "hover:bg-gray-200"
-                    }`}
-                  >
-                    {contact.firstName} {contact.lastName}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          ) : (
-            <p class="text-gray-500 italic">No Contacts</p>
-          )}
-        </section>
+      <section class="flex-1 overflow-y-auto px-2 py-4 md:px-6">
+        {contacts.value.length ? (
+          <ul class="flex h-full flex-col space-y-2">
+            {contacts.value.map((contact) => (
+              <li key={contact.id}>
+                <Link
+                  href={`/contacts/${contact.id}`}
+                  class={`block rounded-md px-2 py-2  ${
+                    loc.url.pathname === `/contacts/${contact.id}/`
+                      ? "bg-blue-500 text-white "
+                      : "hover:bg-gray-200"
+                  }`}
+                >
+                  {contact.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p class="italic text-gray-500">No Contacts</p>
+        )}
+      </section>
 
-        <footer class="py-4 flex space-x-2 items-center px-2 md:px-6">
-          <Link class="flex space-x-2 items-center flex-1" href="/">
-            <img
-              src="https://qwik.builder.io/logos/qwik-logo.svg"
-              class="w-6 h-6 flex-none"
-              loading="lazy"
-              alt=""
-            />
-            <span class="font-medium text-sm">Qwikcity Contacts</span>
-          </Link>
-          <button
-            class="block md:hidden"
-            onClick$={() => (drawer.value = false)}
-          >
-            <ChevronLeftIcon />
-          </button>
-        </footer>
-      </aside>
-    );
-  }
-);
+      <footer class="flex items-center space-x-2 px-2 py-4 md:px-6">
+        <Link class="flex flex-1 items-center space-x-2" href="/">
+          <img
+            src="https://qwik.builder.io/logos/qwik-logo.svg"
+            class="h-6 w-6 flex-none"
+            loading="lazy"
+            alt=""
+          />
+          <span class="text-sm font-medium">Qwikcity Contacts</span>
+        </Link>
+        <button class="block md:hidden" onClick$={() => (drawer.value = false)}>
+          <ChevronLeftIcon />
+        </button>
+      </footer>
+    </aside>
+  );
+});
--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -10,24 +10,14 @@ export const useContacts = routeLoader$(async ({ url }) => {
   const contacts = await prisma.contact.findMany({
     where: search?.trim().length
       ? {
-          OR: [
-            {
-              firstName: {
-                contains: search,
-              },
-            },
-            {
-              lastName: {
-                contains: search,
-              },
-            },
-          ],
+          name: {
+            contains: search,
+          },
         }
       : undefined,
     select: {
       id: true,
-      firstName: true,
-      lastName: true,
+      name: true,
     },
   });
 
@@ -40,15 +30,14 @@ export const useSearch = routeAction$(
   },
   zod$({
     search: z.string().min(1),
-  })
+  }),
 );
 export default component$(() => {
-  const contactsSignal = useContacts();
   const drawer = useSignal(false);
   return (
     <div class="flex">
-      <Sidebar contacts={contactsSignal.value} drawer={drawer} />
-      <div class="block md:hidden absolute top-4 left-4">
+      <Sidebar drawer={drawer} />
+      <div class="absolute left-4 top-4 block md:hidden">
         <button onClick$={() => (drawer.value = true)}>
           <MenuIcon />
         </button>
